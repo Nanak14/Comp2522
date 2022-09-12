b799922b065169d8c6bc54863c42e41bd765bcee
Nanakpreet Singh <snanak1403@gmail.com>
2022-09-12 14:12:45 -0700

    Corrected the checkstyle errors.

--- src/COMP2522/java/org/example/Main.java
+++ src/COMP2522/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
  * This is a class with hello world message.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("This is my world");
-    }
+  public static void main(String[] args) {
+    System.out.println("This is my world");
+  }
 }
